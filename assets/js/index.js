f74c9a9f85b5d568b92064e946439ca6633f93d1
Scottie <avscott@pacbell.net>
2019-12-02 00:12:23 -0800

    add carousel for projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
      // to open side nav menu when navbar links reduce to hamburger menu
      $(".sidenav").sidenav();
 
-     
+     // initialize carousel
+     $('.carousel').carousel();
 });
